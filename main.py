4bd43d6d6bf8b03ae2c06f4ec81c8b43dbaf12d5
Deanisgarayev <garaevdenis2019@gmail.com>
2024-12-11 12:53:33 +0500

    done

---
+++ main.py
@@ -0,0 +1,19 @@
+def calulator(a=int(input()), sign=str(input()), b=int(input())):
+    if sign=="+":
+        print(a+b)
+    elif sign=="-":
+        print(a-b)
+    elif sign=="*":
+        print(a*b)
+    elif sign=="/":
+        print(a/b)
+    else:
+        print("can not do this try another sign")
+try:
+    calulator()
+except ZeroDivisionError:
+    print("you are not allowed divide by zero")
+e
+
+
+
